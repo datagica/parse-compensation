5889bcb51d9698c64136c1b9e6d70fd257bb5a75
Julian Bilcke <julian.bilcke@gmail.com>
2016-04-09 14:05:23 +0200

    debugged base functions

--- tests/all.js
+++ tests/all.js
@@ -2,37 +2,181 @@ const chai = require('chai')
 chai.use(require('chai-fuzzy'))
 const expect = chai.expect
 
-const parseCompensation = require("../lib/parse-compensation");
+const ParseCompensation = require("../lib/parse-compensation").ParseCompensation;
+const parseCompensation = new ParseCompensation();
 
 describe('@datagica/parse-compensation', () => {
 
-  describe('compensation', () => {
-    it('should work with english', done => {
+  describe('French:', () => {
 
-      const tests = [
+    it('should parse currencies', done => {
 
-      // dollars
-      {
-        input: "compensation: $80K/mo",
-        output: [{currency:"USD",yearly:80000}]
-      },
-
-      // pounds
-      {
-        input: "salary: £42K/mo",
-        output: [{currency:"GBP",yearly:42000}]
-      },
-
-      // decimal number
-      {
-        input: "salary: £55.5K/mo",
-        output: [{currency:"GBP",yearly:55500}]
-      },
-    ]
+      const tests = [{
+        input: {
+          amount: "2200€/mo",
+          locale: "en"
+        },
+        output: "EUR"
+      }, {
+        input: {
+          amount: "1300€ par mois",
+          locale: "fr"
+        },
+        output: "EUR"
+      }, {
+        input: {
+          amount: "$4000 / mois",
+          locale: "en"
+        },
+        output: "USD"
+      }, {
+        input: {
+          amount: "£5000 par mois",
+          locale: "en"
+        },
+        output: "GBP"
+      }]
 
       Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
+        const output = parseCompensation.parseCurrency(test.input.amount, test.input.locale)
+        //console.log("output: " + JSON.stringify(output));
+        expect(output).to.be.like(test.output)
+        return Promise.resolve(true)
+      })).then(ended => {
+        //console.log(`test ended`)
+        done()
+        return true
+      }).catch(exc => {
+        console.error(exc)
+      })
+
+    })
+    it('should parse singly values (daily)', done => {
+
+      const tests = [{
+        input: {
+          amount: "10€/heure",
+          locale: "fr",
+          currency: "EUR"
+        },
+        output: 10
+      }, {
+        input: {
+          amount: "15€ de l'heure",
+          locale: "fr",
+          currency: "EUR"
+        },
+        output: 15
+      }, {
+        input: {
+          amount: "$25 / hour",
+          locale: "en",
+          currency: "USD"
+        },
+        output: 25
+      }, {
+        input: {
+          amount: "£30 per hour",
+          locale: "en",
+          currency: "GBP"
+        },
+        output: 30
+      }]
+
+      Promise.all(tests.map(test => {
+        const output = parseCompensation.parseValue(test.input.amount, test.input.locale, test.input.currency)
+        //console.log("output: " + JSON.stringify(output));
+        expect(output).to.be.like(test.output)
+        return Promise.resolve(true)
+      })).then(ended => {
+        //console.log(`test ended`)
+        done()
+        return true
+      }).catch(exc => {
+        console.error(exc)
+      })
+
+
+    })
+    it('should parse single values (monthly)', done => {
+
+      const tests = [{
+        input: {
+          amount: "2500€/mois",
+          locale: "fr",
+          currency: "EUR"
+        },
+        output: 2500
+      }, {
+        input: {
+          amount: "2k€ par mois",
+          locale: "fr",
+          currency: "EUR"
+        },
+        output: 2000
+      }, {
+        input: {
+          amount: "$4000 / mois",
+          locale: "en",
+          currency: "USD"
+        },
+        output: 4000
+      }, {
+        input: {
+          amount: "£5000 par mois",
+          locale: "en",
+          currency: "GBP"
+        },
+        output: 5000
+      }]
+
+      Promise.all(tests.map(test => {
+        const output = parseCompensation.parseValue(test.input.amount, test.input.locale, test.input.currency)
+        //console.log("output: " + JSON.stringify(output));
+        expect(output).to.be.like(test.output)
+        return Promise.resolve(true)
+      })).then(ended => {
+        //console.log(`test ended`)
+        done()
+        return true
+      }).catch(exc => {
+        console.error(exc)
+      })
+
+
+    })
+
+    it('should parse complex sentences', done => {
+
+      const tests = [
+        // french
+        {
+          input: "salaire mensuel : 2500€",
+          output: {
+            hourly: {
+              currency:"EUR",
+              value:16
+            },
+            monthly:{
+              currency:"EUR",
+              value:2500
+            }
+          }
+        }, {
+          input: "salaire : 4K€ par mois",
+          output: {}
+        }, {
+          input: "salaire : 8K€ / mois",
+          output: {}
+        }, {
+          input: "rémunération : 10 K€ / mois",
+          output: {}
+        }
+      ]
+
+      Promise.all(tests.map(test => {
+        return parseCompensation.parse(test.input).then(output => {
+          console.log("output: " + JSON.stringify(output));
           expect(output).to.be.like(test.output)
           return Promise.resolve(true)
         })
@@ -44,189 +188,7 @@ describe('@datagica/parse-compensation', () => {
         console.error(exc)
       })
     })
-    it('should work with french', done => {
 
-      const tests = [
-      // french
-      {
-        input: "salaire mensuel : 32K€",
-        output: [{currency:"EUR",yearly:32000}]
-      },
-      {
-        input: "salaire : 30K € par mois",
-        output: [{currency:"EUR",yearly:30000}]
-      },
-      {
-        input: "salaire brut : 80K € / mois",
-        output: [{currency:"EUR",yearly:80000}]
-      },
-      {
-        input: "rémunération : 50 K€ / mois",
-        output: [{currency:"EUR",yearly:50000}]
-      }
-    ]
-
-      Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
-          expect(output).to.be.like(test.output)
-          return Promise.resolve(true)
-        })
-      })).then(ended => {
-        //console.log(`test ended`)
-        done()
-        return true
-      }).catch(exc => {
-        console.error(exc)
-      })
-    })
-    it('should work with chinese', done => {
-
-      const tests = [
-      {
-        input: "工钱 350K CNY",
-        output: [{currency: "CNY", yearly: 350000}]
-      },
-      {
-        input: "报酬 370K CNY",
-        output: [{currency: "CNY", yearly: 370000}]
-      }
-    ]
-
-      Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
-          expect(output).to.be.like(test.output)
-          return Promise.resolve(true)
-        })
-      })).then(ended => {
-        //console.log(`test ended`)
-        done()
-        return true
-      }).catch(exc => {
-        console.error(exc)
-      })
-    })
-    it('should work with japanese', done => {
-
-      const tests = [
-      {
-        input: "報酬 ¥ 7M",
-        output: [{currency:"JPY",yearly:7000000}]
-      }
-    ]
-
-      Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
-          expect(output).to.be.like(test.output)
-          return Promise.resolve(true)
-        })
-      })).then(ended => {
-        //console.log(`test ended`)
-        done()
-        return true
-      }).catch(exc => {
-        console.error(exc)
-      })
-    })
-
-    it('should work with russian', done => {
-
-      const tests = [
-      {
-        input: "зарплата 3M RUB",
-        output: [{currency: "RUB", yearly: 3000000}]
-      },
-      {
-        input: "зарплата 2.5M ₽",
-        output: [{currency: "RUB", yearly: 2500000}]
-      }
-    ]
-
-      Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
-          expect(output).to.be.like(test.output)
-          return Promise.resolve(true)
-        })
-      })).then(ended => {
-        //console.log(`test ended`)
-        done()
-        return true
-      }).catch(exc => {
-        console.error(exc)
-      })
-    })
-
-    it('should work with spanish', done => {
-
-      const tests = [
-      {
-        input: "salario 450K MXN",
-        output: [{currency: "MXN", yearly: 450000}]
-      },
-      {
-        input: "sueldo 900K Mex$",
-        output: [{currency: "MXN", yearly: 900000}]
-      }
-    ]
-
-      Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
-          expect(output).to.be.like(test.output)
-          return Promise.resolve(true)
-        })
-      })).then(ended => {
-        //console.log(`test ended`)
-        done()
-        return true
-      }).catch(exc => {
-        console.error(exc)
-      })
-    })
-
-
-    it('should work with portuguese', done => {
-
-      const tests = [
-      {
-        input: "salário 35K €",
-        output: [{currency: "EUR", yearly: 35000}]
-      },
-      {
-        input: "remuneração 38K€",
-        output: [{currency: "EUR", yearly: 38000}]
-      },
-      {
-        input: "remunerações R$ 75K",
-        output: [{currency: "BRL", yearly: 75000}]
-      },
-      {
-        input: "vencimento R$ 100K",
-        output: [{currency: "BRL", yearly: 100000}]
-      },
-      {
-        input: "ordenado R$ 150K",
-        output: [{currency: "BRL", yearly: 150000}]
-      }
-    ]
-
-      Promise.all(tests.map(test => {
-        return parseCompensation(test.input).then(output => {
-          //console.log("output: " + JSON.stringify(output));
-          expect(output).to.be.like(test.output)
-          return Promise.resolve(true)
-        })
-      })).then(ended => {
-        //console.log(`test ended`)
-        done()
-        return true
-      }).catch(exc => {
-        console.error(exc)
-      })
-    })
   })
 
 })
